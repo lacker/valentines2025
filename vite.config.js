ad780852e3529c0c12b278567c38f1a8a20cca80
Kevin Lacker <lacker@gmail.com>
2025-02-12 10:40:49 -0800

    ngrok ok

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: '*.ngrok-free.app'
+    },
+    allowedHosts: true
+  }
 })
